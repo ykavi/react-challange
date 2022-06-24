3ad4950c4ba28b4e47fa4578d359c75fa8f01627
Kavi <ykavi@ebay.com>
2022-06-24 16:39:36 +0300

    added tab mock data

---
+++ mock/index.js
@@ -0,0 +1,21 @@
+export const TAB_DATA = [
+  {
+    title: 'Sıralama',
+    subTitle: [
+      { title: 'En Düşük Fiyat', key: 'down' },
+      { title: 'En Yüksek Fiyat', key: 'up' },
+    ],
+  },
+  {
+    title: 'Marka',
+    subTitle: [
+      { title: 'Samsung', key: 'down' },
+      { title: 'Nokia', key: 'up' },
+      { title: 'Apple', key: 'up' },
+      { title: 'LG', key: 'up' },
+      { title: 'Huawei', key: 'up' },
+      { title: 'Xiaomi', key: 'up' },
+      { title: 'General Mobile', key: 'up' },
+    ],
+  },
+];
