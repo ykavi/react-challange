e73f4ed2289318d21108b013b148711cfd27948c
Kavi <ykavi@ebay.com>
2022-06-26 02:24:49 +0300

    added search func

--- components/SearchBar/index.js
+++ components/SearchBar/index.js
@@ -1,16 +1,35 @@
-import { useTheme } from 'styled-components';
 import { StyledInput, Container, IconWrapper } from './style';
 import { Search } from '@Icons';
+import { PRODUCT_LIST } from '@mock';
+import { useDispatch } from 'react-redux';
+import { setProducts } from '../../redux/actions/main';
+import { useEffect, useState } from 'react';
 
 const SearchBar = () => {
-  const theme = useTheme();
+  const dispatch = useDispatch();
+  const [inputValue, setInputValue] = useState('');
+  const [filteredProducts, setFilteredProducts] = useState();
+
+  useEffect(() => {
+    const filteredItems = PRODUCT_LIST.filter((item) => item.title.includes(inputValue));
+    setFilteredProducts(filteredItems);
+  }, [inputValue]);
+
+  useEffect(() => {
+    if (inputValue.length > 2) dispatch(setProducts(filteredProducts));
+  }, [filteredProducts]);
+
+  const onChangeHandle = (e) => {
+    const { value } = e.target;
+    setInputValue(value);
+  };
 
   return (
     <Container>
       <IconWrapper>
         <Search width={17} />
       </IconWrapper>
-      <StyledInput type="text" placeholder="25 milyon’dan fazla ürün içerisinde ara" />
+      <StyledInput onChange={onChangeHandle} type="text" placeholder="25 milyon’dan fazla ürün içerisinde ara" />
     </Container>
   );
 };
