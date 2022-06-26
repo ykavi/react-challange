f0f90b5b37879383c1b8cbcce359ada03ae01ec8
Kavi <ykavi@ebay.com>
2022-06-26 20:33:02 +0300

    initial filter feature

--- pages/index.js
+++ pages/index.js
@@ -1,31 +1,49 @@
 import { Col, Row } from 'react-styled-flexboxgrid';
 import { PageTitle, FilterTab, SelectBox, Text, ProductList, Pagination } from '@components';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { setProducts } from '../redux/actions/main';
+import { PRODUCT_LIST } from '@mock';
 
-const Home = () => (
-  <>
-    <Row>
-      <Col lg={20} md={20} sm={20} xs={20}>
-        <PageTitle />
-      </Col>
+const Home = () => {
+  const dispatch = useDispatch();
+  const router = useRouter();
+  const { filterKey } = router.query;
 
-      <Col lg={4} md={4} sm={4} xs={4}>
-        <SelectBox>
-          <Text color="grey_v5">Sıralama</Text>
-        </SelectBox>
-      </Col>
-    </Row>
+  useEffect(() => {
+    if (filterKey) {
+      const filteredData = PRODUCT_LIST.filter((item) => item.brand?.toLowerCase() === filterKey?.toLowerCase());
+      dispatch(setProducts(filteredData));
+    }
+  }, [filterKey]);
 
-    <Row>
-      <Col lg={4} md={4} sm={4} xs={0}>
-        <FilterTab />
-      </Col>
+  return (
+    <>
+      <Row>
+        <Col lg={20} md={20} sm={20} xs={20}>
+          <PageTitle />
+        </Col>
 
-      <Col lg={20} md={20} sm={20} xs={23}>
-        <ProductList />
-        <Pagination />
-      </Col>
-    </Row>
-  </>
-);
+        <Col lg={4} md={4} sm={4} xs={4}>
+          <SelectBox>
+            <Text color="grey_v5">Sıralama</Text>
+          </SelectBox>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col lg={4} md={4} sm={4} xs={0}>
+          <FilterTab />
+        </Col>
+
+        <Col lg={20} md={20} sm={20} xs={23}>
+          <ProductList />
+          <Pagination />
+        </Col>
+      </Row>
+    </>
+  );
+};
 
 export default Home;
