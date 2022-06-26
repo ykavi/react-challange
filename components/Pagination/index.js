72d7c4a812de8d5c9b51269681b85130034ae0be
Kavi <ykavi@ebay.com>
2022-06-27 00:00:07 +0300

    added pagination feature

--- components/Pagination/index.js
+++ components/Pagination/index.js
@@ -1,11 +1,30 @@
 import { Container, PageItem } from './style';
+import { paginate } from '@helpers';
+import { PRODUCT_LIST } from '@mock';
+import { useDispatch } from 'react-redux';
+import { setProducts } from '../../redux/actions/main';
+import { useEffect, useState } from 'react';
 
 const Pagination = () => {
+  const dispatch = useDispatch();
+  const pages = paginate(PRODUCT_LIST, 8);
+  const [currentPage, setCurrentPage] = useState(0);
+
+  useEffect(() => {
+    dispatch(setProducts(pages[currentPage]));
+  }, [currentPage]);
+
+  const onClickHandle = (index) => {
+    setCurrentPage(index);
+  };
+
   return (
     <Container>
-      <PageItem isActive>1</PageItem>
-      <PageItem>2</PageItem>
-      <PageItem>3</PageItem>
+      {pages?.map((item, index) => (
+        <PageItem key={index} isActive={index === currentPage} onClick={() => onClickHandle(--index)}>
+          {++index}
+        </PageItem>
+      ))}
     </Container>
   );
 };
--- utils/helpers.js
+++ utils/helpers.js
@@ -57,3 +57,13 @@ export const getSelectBoxText = (isLowSort, isHighSort) => {
 
   return `Sıralama`;
 };
+
+export const paginate = (arr, size) => {
+  return arr.reduce((acc, val, i) => {
+    let idx = Math.floor(i / size);
+    let page = acc[idx] || (acc[idx] = []);
+    page.push(val);
+
+    return acc;
+  }, []);
+};
